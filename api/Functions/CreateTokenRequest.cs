731d221229e292af1e6b1365960971ba4619843b
Marc Duiker <marcduiker@gmail.com>
2022-05-16 15:13:19 +0200

    Add CreateTokenRequest function

---
+++ api/Functions/CreateTokenRequest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using IO.Ably;
+using System.Net.Http;
+
+namespace AblyLabs.ServerlessWebsocketsQuest
+{
+    public class CreateTokenRequest
+    {
+        private AblyRealtime _realtime;
+
+        public CreateTokenRequest(AblyRealtime realtime)
+        {
+            _realtime = realtime;
+        }
+
+        [FunctionName(nameof(CreateTokenRequest))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequestMessage req,
+            ILogger log)
+        {
+            var tokenParams = new TokenParams() { ClientId = Guid.NewGuid().ToString() };
+            var tokenData = await _realtime.Auth.CreateTokenRequestAsync(tokenParams);
+
+            return new OkObjectResult(tokenData);
+        }
+    }
+}
