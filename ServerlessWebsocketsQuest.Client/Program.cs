feec3d46e9f308883ec75b6917c0de89cff85a5d
Marc Duiker <marcduiker@gmail.com>
2022-06-01 14:39:52 +0200

    Inject IRealtimeClient

--- ServerlessWebsocketsQuest.Client/Program.cs
+++ ServerlessWebsocketsQuest.Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using IO.Ably;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IRealtimeClient>(sp => new AblyRealtime(builder.Configuration["ABLY_API_KEY"]));
 
 await builder.Build().RunAsync();
