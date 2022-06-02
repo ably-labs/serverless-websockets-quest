908974c94227ff5175b1e5f88e5ff99735f27f1b
Marc Duiker <marcduiker@gmail.com>
2022-06-02 11:42:05 +0200

    Start with Vue TS app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HomePage from './components/HomePage.vue'
+</script>
+
 <template>
   <HomePage />
 </template>
 
-<script>
-import HomePage from "./components/HomePage.vue";
-
-export default {
-  name: "App",
-  components: {
-    HomePage,
-  }
-};
-</script>
-
 <style>
+
+#app {
+  font-family: 'Courier New', Courier, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: larger;
+  font-weight: bold;
+  text-align: center;
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+  margin: 0;
+}
+
+a {
+  color: #55ffff;
+}
+
+.small {
+  width: 150px;
+}
+
+button {
+  font-family: 'Courier New', Courier, monospace;
+  font-weight: bold;
+  font-size: larger;
+  color: #000;
+  background-color: #ff55ff;
+  border-color: #fff;
+  padding: 10px;
+  margin: 10px;
+}
 
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,46 +1,29 @@
-<template>
-  <div>
-    <h1>{{ getTitle }}</h1>
-    <button
-      :disabled="!isDefaultTitle"
-      @click="setTitleViaApi"
-    >
-      Set title via API
-    </button>
-    <button
-      :disabled="isDefaultTitle"
-      @click="resetTitle"
-    >
-      Reset title
-    </button>
-    <p>Use <a href="https://github.com/marcduiker/staticwebapp-vue-vite">this GitHub template repo</a> to make a cool <a href="https://docs.microsoft.com/azure/static-web-apps/">Azure Static Web App</a> yourself! 🚀</p>
-  </div>
-</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import FooterSection from './FooterSection.vue';
 
-<script>
-
-import { mapGetters, mapActions } from "vuex";
-
-export default {
-  components: {
-  },
-   computed: {
-    ...mapGetters([
-        "getTitle",
-        "isDefaultTitle"
-      ])
-  },
-  methods: {
-    ...mapActions([
-      "setTitleViaApi",
-      "resetTitle"
-    ]),
-  },
-};
+const count = ref(0)
 </script>
 
-<style scope>
-body {
-  font-family: 'Comic Sans MS'
-}
+<template>
+  <h1>Serverless Websockets Quest</h1>
+  <img class="small" alt="monster" src="../assets/Monster.png" />
+  <img class="small" alt="monster" src="../assets/Fighter.png" />
+  <img class="small" alt="monster" src="../assets/Ranger.png" />
+  <img class="small" alt="monster" src="../assets/Mage.png" />
+
+  <p>
+    Start a new quest or join an existing quest and provide the quest ID.
+  </p>
+
+  <button @click="foo">Start new quest</button>
+  <button @click="foo">Join quest</button>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  
+  <FooterSection />
+</template>
+
+<style scoped>
+
 </style>
